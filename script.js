ebc45f905c7158142f2409a3ddf2b70a98a546c9
vpalani462-sketch <vpalani462@gmail.com>
2026-07-15 20:23:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,51 @@ row.insertCell(3).innerHTML = amount;
 document.getElementById("grandTotal").innerHTML = total;
 
 }
+function saveCustomer(){
+
+let name = document.getElementById("customerName").value;
+let mobile = document.getElementById("mobile").value;
+let address = document.getElementById("address").value;
+
+let customer = {
+name:name,
+mobile:mobile,
+address:address
+};
+
+let customers = JSON.parse(localStorage.getItem("customers")) || [];
+
+customers.push(customer);
+
+localStorage.setItem("customers", JSON.stringify(customers));
+
+alert("Customer Saved");
+
+showCustomers();
+
+}
+
+
+function showCustomers(){
+
+let list = document.getElementById("customerList");
+
+if(!list) return;
+
+let customers = JSON.parse(localStorage.getItem("customers")) || [];
+
+list.innerHTML="";
+
+customers.forEach(c=>{
+
+list.innerHTML += `
+<p>
+${c.name} - ${c.mobile}
+</p>
+`;
+
+});
+
+}
+
+showCustomers();
